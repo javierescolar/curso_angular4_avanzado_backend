59fe13c1b7a7d8ec2bf3565c3d9860f6ea6231f9
Javier Escolar Salcedo <javierescolar10@gmail.com>
2017-11-07 19:32:20 +0100

    API RESTful terminada

--- controllers/animal_controller.js
+++ controllers/animal_controller.js
@@ -86,7 +86,7 @@ function getAnimal(req, res) {
           message:'animal no encontrado'
         });
       } else {
-        res.status(404).send({
+        res.status(200).send({
           message:'animal encontrado',
           animal: animal
         });
@@ -95,11 +95,122 @@ function getAnimal(req, res) {
   });
 }
 
+function updateAnimal(req,res) {
+  var animalId = req.params.id;
+  var update = req.body;
+
+  Animal.findByIdAndUpdate(animalId,update,{new:true},(err, animalUpdated) => {
+    if (err) {
+      res.status(500).send({message:'Error en la petición'});
+    } else {
+      if (!animalUpdated) {
+        res.status(404).send({message:'No se ha updateado el animal'});
+      } else {
+        res.status(200).send({
+          message:'animal modificado',
+          animal: animalUpdated
+        });
+      }
+    }
+  });
+}
+
+function uploadImage(req,res){
+  var animalId = req.params.id;
+  var file_name = "No subido...";
+
+  if(req.files){
+    var file_path = req.files.image.path;
+    var file_split = file_path.split('/');
+    var file_name = file_split[2];
+    var ext_split = file_name.split('.');
+    var file_ext = ext_split[1];
+
+    if(file_ext == 'png' || file_ext == 'jpg'
+      || file_ext == 'jpeg' || file_ext == 'gif'){
+
+
+      Animal.findByIdAndUpdate(animalId,{image: file_name},{new:true},(err, animalUpdated)=> {
+        if(err){
+          res.status(500).send({
+            message: 'Error al updatear imagen del animal'
+          });
+        } else {
+          res.status(200).send({
+            message: 'animal actualizado con la imagen',
+            animal:animalUpdated, image:file_name
+          });
+        }
+      });
+
+
+    } else {
+      fs.unlink(file_path,(err)=>{
+        if(err){
+          res.status(200).send({
+            message: 'Extensión no válida y fichero no guardado'
+          });
+        } else {
+          res.status(200).send({
+            message: 'Extensión no válida'
+          });
+        }
+      });
+    }
+  } else {
+    res.status(200).send({
+      message: 'No se han subido ficheros'
+    });
+  }
+}
+
+
+function getImageFile(req,res){
+  var imageFile = req.params.imageFile;
+  var path_file = path_files_uploads + imageFile;
+
+  fs.exists(path_file, (exists) => {
+    if(exists){
+      res.sendFile(path.resolve(path_file));
+    } else {
+      res.status(404).send({
+        message: 'Imagen no existe'
+      });
+    }
+  });
+}
+
+function deleteAnimal(req,res){
+  var animalId = req.params.id;
+
+  Animal.findByIdAndRemove(animalId,(err,animalRemoved)=>{
+    if (err) {
+      res.status(500).send({
+        message: 'Error en la petición'
+      });
+    } else {
+      if (!animalRemoved) {
+        res.status(404).send({
+          message: 'No se ha podido borrar el animal'
+        });
+      } else {
+        res.status(200).send({
+          message: 'Animal eliminado',
+          animalRemoved
+        });
+      }
+    }
+  });
+}
 
 
 module.exports = {
   pruebas,
   saveAnimal,
   getAnimals,
-  getAnimal
+  getAnimal,
+  updateAnimal,
+  uploadImage,
+  getImageFile,
+  deleteAnimal
 }
--- routes/animal_routes.js
+++ routes/animal_routes.js
@@ -5,15 +5,20 @@ const AnimalController = require('../controllers/animal_controller');
 var api = express.Router();
 
 const middlewareAuth = require('../middlewares/authenticate');
+const middlewareAdmin = require('../middlewares/is_admin');
 const multiparty = require('connect-multiparty');
 var middlewareUpload = multiparty({uploadDir: './uploads/animals'});
 
 
 //rutas
 api.get('/pruebas-animales', middlewareAuth.ensureAuth,AnimalController.pruebas);
-api.post('/animal', middlewareAuth.ensureAuth,AnimalController.saveAnimal);
+api.post('/animal', [middlewareAuth.ensureAuth, middlewareAdmin.isAdmin],AnimalController.saveAnimal);
 api.get('/animals', AnimalController.getAnimals);
 api.get('/animal/:id', AnimalController.getAnimal);
+api.put('/animal/:id', [middlewareAuth.ensureAuth, middlewareAdmin.isAdmin], AnimalController.updateAnimal);
+api.post('/upload-image-animal/:id', [middlewareAuth.ensureAuth,middlewareAdmin.isAdmin,middlewareUpload],AnimalController.uploadImage);
+api.get('/get-image-animal/:imageFile', AnimalController.getImageFile);
+api.delete('/animal/:id', [middlewareAuth.ensureAuth, middlewareAdmin.isAdmin], AnimalController.deleteAnimal);
 
 
 module.exports = api;
